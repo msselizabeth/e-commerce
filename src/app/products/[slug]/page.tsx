4967b514a36977899a633a32b59c948d91909e8f
Elizabeth <lizagotovitryby@gmail.com>
2025-01-17 14:08:30 -0500

    updated Product page with API requset , passed prod to DetailedProduct component

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,10 +1,40 @@
+import DetailedProduct from "@/components/DetailedProduct/DetailedProduct";
 
-const ProductPage = () => { 
-    return (
-        <div>
-      
-        </div>
-    );
+interface Product {
+  id: string;
+  productName: string;
+  description: string;
+  price: number;
+  image: string;
+  stock: number;
+  wax: string;
+  fragrance: string;
+  dimension: string;
+  weight: string;
 }
 
-export default ProductPage;
+
+async function getProduct(id: string) {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/products/${id}`, {
+    cache: "force-cache",
+  });
+  const result = await res.json();
+  const prod: Product = result.product;
+  if (!prod) {
+    console.error("Error");
+  }
+  return prod;
+}
+
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const item = await getProduct(slug);
+
+    return (
+      <DetailedProduct product={item}/>
+  );
+}
